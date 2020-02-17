70601629cec8a107a9df11019c00ed1eb67235cc
Emrah <komnatadeveloper@gmail.com>
2020-02-17 14:54:03 +0300

    Current Status before splitting app into widgets

---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import './transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id:'t1',
+      title:'New Shoes',
+      amount: 69.99,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id:'t2',
+      title:'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now()
+    ),
+  ];
+
+  // String titleInput;
+  // String amountInput;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        
+        children: <Widget>[
+
+        // Chart
+        Container(
+          width: double.infinity,
+          child: Card(
+            color: Colors.blue,
+            child: Text('CHART'),              
+            elevation: 5,
+          ),
+        ), 
+
+        Card(
+          elevation: 5,
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget> [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Title'
+                  ),
+                  // onChanged: ( val ) {
+                  //   titleInput = val;
+                  // },
+                  controller: titleController,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Amount'
+                  ),
+                  // onChanged: ( val ) {
+                  //   amountInput = val;
+                  // },
+                  controller: amountController,
+                ),
+                FlatButton(
+                  textColor: Colors.purple,
+                  child: Text('Add Transaction'),
+                  onPressed: () {
+                    // print(titleInput);
+                    // print(amountInput);
+                    print(titleController.text);
+                    print(amountController.text);
+                  },
+                )
+              ]
+            ),
+          ),
+        ),
+
+
+        // Transactions Cards
+        Column(
+          children: transactions.map( (transactionItem) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+
+                  // Amount
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,                        
+                      )
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text( 
+                      '\$${transactionItem.amount}' ,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple
+                      ),
+
+                    ),
+                  ),
+
+                  // RIGHT SIDE
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget> [
+                      Text(
+                        transactionItem.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold
+                        ),  
+                      ),
+                      Text(
+                        // DateFormat('yyyy-MM-dd').format(transactionItem.date) ,   this is also possible with -()dash
+                        // DateFormat('yyyy/MM/dd').format(transactionItem.date) ,   "/" between year month etc
+                        DateFormat.yMMMd().format(transactionItem.date) ,   // this is a preformatted style
+                        style: TextStyle(
+                          color: Colors.grey
+                        ),  
+                      ),
+                    ]
+                  )
+                ],
+              )
+            );
+          }).toList()
+        )
+        
+      ],)
+    );
+  }
+}
